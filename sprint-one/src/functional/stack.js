c18aec4ab32eff1ad5c6f6645016fa140a139998
NicholasMiron <nicholasmiron@gmail.com>
2019-02-20 12:40:52 -0600

    Added functional class implementation for stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,29 @@
 var Stack = function() {
   var someInstance = {};
+  // declare size variable
+  var stackSize = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[stackSize] = value;
+    stackSize++;
   };
 
   someInstance.pop = function() {
+    var poppedValue;
+    if (stackSize > 0) {
+      stackSize--;
+      poppedValue = storage[stackSize];
+      delete storage[stackSize];
+    }
+    return poppedValue;
   };
 
   someInstance.size = function() {
+    return stackSize;
   };
 
   return someInstance;
