3ffe068dd2b5f07b62f79299d3da2734b4c353af
Nicholas Miron <nicholasmiron@gmail.com>
2019-02-21 09:40:46 -0600

    Added a queue implementation for es6 classes

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.queueSize = 0;
+    this.storage = {};
+  }
+  size() {
+    return this.queueSize;
+  }
+  enqueue(value) {
+    this.storage[this.queueSize] = value;
+    this.queueSize++;
+  }
+  dequeue() {
+    if(this.queueSize > 0) {
+      var firstInLine = this.storage[0];
+      delete this.storage[0];
+      this.queueSize--;
+      for(var key in this.storage) {
+        this.storage[key - 1] = this.storage[key];
+      }
+      return firstInLine;
+    }
   }
 
 }
