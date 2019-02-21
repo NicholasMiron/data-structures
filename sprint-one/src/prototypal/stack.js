ec2c38a3e5c2ce97940267850cd1564e14ef091b
Nicholas Miron <nicholasmiron@gmail.com>
2019-02-21 08:41:01 -0600

    Updated stack for prototypal class to pass the tests "Using ARGV"

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,70 +1,24 @@
 var Stack = function() {
-  //Blog Example uses Stack.prototype also needs var stackMethods = {};
-  //Medium Example Uses stackMethods
-  //Giraffe Example Uses Stack.stuff
-
-  var newStack = Object.create(Stack.stuff);
+  var newStack = Object.create(stackMethods);
   newStack.stackSize = 0;
   newStack.storage = {};
   return newStack;
 };
 
-// Medium Example (Change create variable to stuff) PASSES
-// var stuff = {
-//   size : function() {
-//     return this.stackSize;
-//   },
-//   push : function(value) {
-//     this.storage[this.stackSize] = value;
-//     this.stackSize++;
-//   },
-//   pop : function() {
-//     if(this.stackSize > 0) {
-//       this.stackSize--;
-//       var popped = this.storage[this.stackSize];
-//       delete this.storage[this.stackSize];
-//       return popped;
-//     }
-//   }
-// };
-
-
-//Blog Example (Change create variable to Stack.prototype) FAILS
-// var stuff = {};
-
-// Stack.prototype.size = function() {
-//   return this.stackSize;
-// }
-
-// Stack.prototype.push = function(value) {
-//   this.storage[this.stackSize] = value;
-//   this.stackSize++;
-// }
-
-// Stack.prototype.pop = function() {
-//   if (this.stackSize > 0) {
-//     this.stackSize--;
-//     var popped = this.storage[this.stackSize];
-//     delete this.storage[this.stackSize];
-//     return popped;
-//   }
-// }
-
-
-//Giraffe Example (Change create variable to Stack.stuff) FAILS
-Stack.stuff = {};
-Stack.stuff.size = function() {
-  return this.stackSize;
-};
-Stack.stuff.push = function(value) {
-  this.storage[this.stackSize] = value;
-  this.stackSize++;
-};
-Stack.stuff.pop = function() {
-  if (this.stackSize > 0) {
-    this.stackSize--;
-    var popped = this.storage[this.stackSize];
-    delete this.storage[this.stackSize];
-    return popped;
+var stackMethods = {
+  size : function() {
+    return this.stackSize;
+  },
+  push : function(value) {
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+  pop : function() {
+    if(this.stackSize > 0) {
+      this.stackSize--;
+      var popped = this.storage[this.stackSize];
+      delete this.storage[this.stackSize];
+      return popped;
+    }
   }
 };
