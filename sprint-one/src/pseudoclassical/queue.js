9b067149b56c9891ce6fa83bc2eb0145f6fea6fe
Nicholas Miron <nicholasmiron@gmail.com>
2019-02-21 08:56:26 -0600

    Added a queue implementation for pseudoclassical classes

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.queueSize = 0;
+  this.storage = {};
+};
+
+Queue.prototype.size = function() {
+  return this.queueSize;
+};
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.queueSize] = value;
+  this.queueSize++;
+};
+Queue.prototype.dequeue = function() {
+  if (this.queueSize > 0) {
+    this.queueSize--;
+    var firstInLine = this.storage[0];
+    delete this.storage[0];
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+    }
+    return firstInLine;
+  }
 };
 
 
