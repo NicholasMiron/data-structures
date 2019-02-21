fdfc94f40ad32ad25206db0e43d40f9862a5042b
Nicholas Miron <nicholasmiron@gmail.com>
2019-02-21 08:23:14 -0600

    Added three different ways to do this with two that don't work???????

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,70 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  //Blog Example uses Stack.prototype also needs var stackMethods = {};
+  //Medium Example Uses stackMethods
+  //Giraffe Example Uses Stack.stuff
+
+  var newStack = Object.create(Stack.stuff);
+  newStack.stackSize = 0;
+  newStack.storage = {};
+  return newStack;
 };
 
-var stackMethods = {};
+// Medium Example (Change create variable to stuff) PASSES
+// var stuff = {
+//   size : function() {
+//     return this.stackSize;
+//   },
+//   push : function(value) {
+//     this.storage[this.stackSize] = value;
+//     this.stackSize++;
+//   },
+//   pop : function() {
+//     if(this.stackSize > 0) {
+//       this.stackSize--;
+//       var popped = this.storage[this.stackSize];
+//       delete this.storage[this.stackSize];
+//       return popped;
+//     }
+//   }
+// };
 
 
+//Blog Example (Change create variable to Stack.prototype) FAILS
+// var stuff = {};
+
+// Stack.prototype.size = function() {
+//   return this.stackSize;
+// }
+
+// Stack.prototype.push = function(value) {
+//   this.storage[this.stackSize] = value;
+//   this.stackSize++;
+// }
+
+// Stack.prototype.pop = function() {
+//   if (this.stackSize > 0) {
+//     this.stackSize--;
+//     var popped = this.storage[this.stackSize];
+//     delete this.storage[this.stackSize];
+//     return popped;
+//   }
+// }
+
+
+//Giraffe Example (Change create variable to Stack.stuff) FAILS
+Stack.stuff = {};
+Stack.stuff.size = function() {
+  return this.stackSize;
+};
+Stack.stuff.push = function(value) {
+  this.storage[this.stackSize] = value;
+  this.stackSize++;
+};
+Stack.stuff.pop = function() {
+  if (this.stackSize > 0) {
+    this.stackSize--;
+    var popped = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    return popped;
+  }
+};
