b273381ac91ef204de571284a39a35e352b3cba0
Nicholas Miron <nicholasmiron@gmail.com>
2019-02-22 08:23:52 -0600

    Implemented a set using the prototypal class

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
+  set.setSize = 0;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+    this.setSize++;
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    this._storage.splice(this._storage.indexOf(item), 1);
+    this.setSize--;
+  }
+};
+
+setPrototype.size = function() {
+  return this.setSize;
 };
 
 /*
