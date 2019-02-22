b483a510fd894413f2829c57a18b70c85bd158f5
Nicholas Miron <nicholasmiron@gmail.com>
2019-02-21 18:06:07 -0600

    Implemented Tree using traditional shared class

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,30 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  _.extend(newTree, treeMethods);
+  newTree.children = [];
+  
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var containsValue = false;
+  if (this.value === target) {
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    containsValue = this.children[i].contains(target);
+    if (containsValue) {
+      return true;
+    }
+  }
+  return false;
 };
 
 
