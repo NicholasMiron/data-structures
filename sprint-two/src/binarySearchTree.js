3947946e40f20beeb5d65ba31d85a66097c4fdcf
Nicholas Miron <nicholasmiron@gmail.com>
2019-02-22 20:01:39 -0600

    Implemented insert function for binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,46 @@
 var BinarySearchTree = function(value) {
+  this.BSTStorage = new BSTNode(value);
+  this.left = 0;
+  this.right = 0;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  var newNode = new BSTNode(value);
+
+  var findEmptyChild = function(currentNode, value) {
+    if (currentNode.value > value) {
+      if (currentNode.children[0] === null) {
+        currentNode.children[0] = newNode;
+      } else {
+        findEmptyChild(currentNode.children[0]);
+      }
+
+    } else if (currentNode.value < value) {
+      if (currentNode.children[1] === null) {
+        currentNode.children[1] = newNode;
+      } else {
+        findEmptyChild(currentNode.children[1]);
+      }
+
+    }
+  };
+  findEmptyChild(this.BSTStorage, value);
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+
+};
+
+
+var BSTNode = function(value) {
+  this.nodeStorage = {};
+  this.value = value;
+  this.children = [null, null];
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
